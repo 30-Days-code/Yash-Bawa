423725e7b030a43113672fceda6308d0aae5f8d4
Yashvishnoi <jivishnoi@gmail.com>
2020-04-27 21:38:25 +0530

    Update the code

--- Day1.cpp
+++ Day1.cpp
@@ -1,14 +1,16 @@
 #include<iostream>
 #include<string.h>
 using namespace std;
-int totalCopies;
+
 class Book{
 	private:
+	int totalCopies;
 	int nbr;//Book Number
 	string name;//Book name
 	string author;//Book Author
 	string publisher;//Book publisher
 	float price;// Book Price
+	int issued=0;//Book issued
 	public:
 	int Issued;// Number of book issued
 	void getData();
@@ -24,6 +26,11 @@ class Book{
 		cin>>name>>author>>publisher;
 		cout<<"Enter book price";
 		cin>>price;
+		int q;
+		cout<<"Enter total copies";
+		cin>>q;
+		totalCopies==q;
+		cout<<"Enter details for next book"<<endl;
 		
 	}
 	void Book::bookIssue()
@@ -35,7 +42,7 @@ class Book{
 		if(i==1){
 		if(totalCopies>0){
 		totalCopies=-totalCopies;
-		cout<<"Book Issued";
+		cout<<"Book Issued: "<<issued++;
 		}
 		}
 		else
@@ -43,8 +50,9 @@ class Book{
 	}
 	void Book::bookReturn()
 	{
-		if(totalCopies<x){
+		if(issued<totalCopies){
 		totalCopies=+totalCopies;
+		issued--;
 		cout<<"Book Returned";
 		}
 	}
@@ -55,25 +63,29 @@ class Book{
 	}
 int main()
 {
-Book b;
-b.getData();
-int q;
-cout<<"Enter total copies";
-cin>>q;
-totalCopies=q;
+int l;
+cout<<"Enter the number to different types of books available "<<"\n";
+cin>>l;
+Book b[l];
+for(int i=0;i<l;i++)
+{
+b[i].getData();
+}
 cout<<"Press 1 for book issue ,Press 2 for book return, press 3 for book info";
 int x;
+for(int i=0;i<l;i++)
+{
 cin>>x;
 switch(x)
 {
-case 1: b.bookIssue();
+case 1: b[i].bookIssue();
         break;
-case 2: b.bookReturn();
+case 2: b[i].bookReturn();
         break;
-case 3: b.bookInfo();
+case 3: b[i].bookInfo();
         break;
 default: cout<<"Wrong choice"<<"\n";
+}
 } 
-cout<<"Total number of copies available"<<totalCopies;
 return 0;
 }
